8f70b3ce0b1cac508a1bb46aa6d37d2abcbedf62
pgs5983 <pgs5983@rit.edu>
2022-09-10 17:32:17 -0400

    Implemented the main program. It runs, calculates, outputs the result properly and accurately. Missing docstrings.

--- src/rit/stu/DYI.java
+++ src/rit/stu/DYI.java
@@ -1,6 +1,44 @@
 package rit.stu;
 
+import rit.cs.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
 public class DYI {
+
+    private Scanner prefix = new java.util.Scanner(System.in);
+
+    public DYI(){
+    }
+
+    public static Expression parse(ArrayList<String> tokens){
+        var tok = tokens.remove(0);
+        return switch (tok) {
+            case "+" -> new AddExpression(parse(tokens), parse(tokens));
+            case "-" -> new SubExpression(parse(tokens), parse(tokens));
+            case "*" -> new MulExpression(parse(tokens), parse(tokens));
+            case "/" -> new DivExpression(parse(tokens), parse(tokens));
+            case "%" -> new ModExpression(parse(tokens), parse(tokens));
+            default -> new IntExpression(Integer.parseInt(tok));
+        };
+    }
     public static void main(String[] args) {
+        DYI expression = new DYI();
+        System.out.println("Derp Your Interpreter!");
+        System.out.print(">");
+        String userInput = expression.prefix.nextLine();
+        while(!userInput.equals("quit")) {
+            String[] str = userInput.split(" ");
+            ArrayList<String> expArrayEval = new ArrayList<>(Arrays.asList(str));
+            ArrayList<String> expArrayEmit = new ArrayList<>(expArrayEval);
+            System.out.println("Emit: " + parse(expArrayEmit).emit());
+            System.out.println("Evaluate: " + parse(expArrayEval).evaluate());
+            System.out.print(">");
+            userInput = expression.prefix.nextLine();
+        }
+        System.out.println("Goodbye!");
     }
 }
